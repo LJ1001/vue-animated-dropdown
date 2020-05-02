7d0cc35cad9d28ac131721ad146fe1a0b3379e0f
LJ1001 <marcel-dambach@gmx.de>
2020-05-02 16:54:27 +0200

    Basic transition between dropdowns

--- src/components/DropdownItem.vue
+++ src/components/DropdownItem.vue
@@ -1,10 +1,11 @@
 <template>
   <a
     href="#"
-    class="h-12 text-gray-300 flex items-center rounded-lg transition-colors duration-300 p-2
+    class="h-12 w-64 text-gray-300 flex items-center rounded-lg transition-colors duration-300 p-2
            hover:bg-gray-600"
   >
-    <span class="">
+    <span class="w-8 h-8 bg-gray-700 rounded-full p-1 ml-1 mr-2 flex justify-center items-center
+            transition duration-300 hover:bg-gray-600">
       <slot name="leftIcon"></slot>
     </span>
 
--- src/components/DropdownMenu.vue
+++ src/components/DropdownMenu.vue
@@ -1,30 +1,48 @@
 <template>
   <div class="absolute w-64 custom-top custom-transform bg-gray-800 overflow-hidden
-              border border-gray-700 border-p-4 rounded-lg">
-    <div v-if="activeMenu === 'main'">
-      <DropdownItem>
-        <template>
-          My Profile
-        </template>
-      </DropdownItem>
+              border border-gray-700 border-p-4 rounded-lg flex">
+    <transition name="menu-primary">
+      <div v-if="activeMenu === 'main'">
+        <DropdownItem>
+          <template>
+            My Profile
+          </template>
+        </DropdownItem>
 
-      <DropdownItem>
-        <template #leftIcon>
-          <Cog />
-        </template>
-        <template>
-          Settings
-        </template>
-        <template #rightIcon>
-          <Chevron />
-        </template>
-      </DropdownItem>
-    </div>
+        <DropdownItem @click.native="setActiveMenu('settings')">
+          <template #leftIcon>
+            <Cog />
+          </template>
+          <template>
+            Settings
+          </template>
+          <template #rightIcon>
+            <Chevron />
+          </template>
+        </DropdownItem>
+      </div>
+    </transition>
+
+    <transition name="menu-secondary">
+      <div v-if="activeMenu === 'settings'">
+        <DropdownItem @click.native="setActiveMenu('main')">
+          <template #leftIcon>
+            <Arrow />
+          </template>
+        </DropdownItem>
+        <DropdownItem >
+          <template>
+            Settings
+          </template>
+        </DropdownItem>
+      </div>
+    </transition>
   </div>
 </template>
 
 <script>
 import DropdownItem from './DropdownItem';
+import Arrow from './icons/Arrow';
 import Cog from './icons/Cog';
 import Chevron from './icons/Chevron';
 
@@ -32,6 +50,7 @@ export default {
   name: 'DropDownMenu',
   components: {
     DropdownItem,
+    Arrow,
     Cog,
     Chevron,
   },
@@ -54,5 +73,47 @@ export default {
   }
   .custom-transform {
     transform: translateX(-45%);
+  }
+  .menu-primary-enter {
+    position: absolute;
+    transform: translateX(-110%);
+  }
+  .menu-primary-enter-to {
+    position: absolute;
+    transform: translateX(0%);
+  }
+  .menu-primary-enter-active {
+    transition: all 0.5s ease;
+  }
+  .menu-primary-leave {
+    position: absolute;
+    transform: translateX(0%);
+  }
+  .menu-primary-leave-to {
+    transform: translateX(-110%);
+  }
+  .menu-primary-leave-active {
+    transition: all 0.5s ease;
+  }
+  .menu-secondary-enter {
+    position: absolute;
+    transform: translateX(110%);
+  }
+  .menu-secondary-enter-to {
+    position: absolute;
+    transform: translateX(0%);
+  }
+  .menu-secondary-enter-active {
+    transition: all 0.5s ease;
+  }
+  .menu-secondary-leave {
+    position: absolute;
+    transform: translateX(0%);
+  }
+  .menu-secondary-leave-to {
+    transform: translateX(110%);
+  }
+  .menu-secondary-leave-active {
+    transition: all 0.5s ease;
   }
 </style>
